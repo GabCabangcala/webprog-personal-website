b3d7ca4535f92ea28405b4e210bb14ecb1418b2c
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2024-12-18 20:41:41 +0800

    added function 2

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,9 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo").innerHTML = "HELLO!";
+  }
+
+  function myFunction2() {
+    const element = document.getElementsByTagName("p");
+    document.getElementById("demo").innerHTML = 'PARAGRAPH CHANGED!!' + element[0].innerHTML;
+  
   }
