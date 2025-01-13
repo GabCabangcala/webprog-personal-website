1e3635d96600867480bfeabaf8b2bd4af641d482
gaboy <149191631+GabCabangcala@users.noreply.github.com>
2025-01-13 15:48:38 +0800

    customize update 1

---
+++ home/vue.js
@@ -0,0 +1,32 @@
+// Define the photo-gallery component globally
+Vue.component('photo-gallery', {
+    template: `
+        <div class="gallery">
+            <div class="gallery-row">
+                <div class="gallery-item" v-for="(image, index) in images" :key="index">
+                    <img :src="image.src" :alt="image.alt" class="img-fluid" />
+                </div>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            images: [
+                { src: 'Images/gf1.jpg', alt: 'Image 1' },
+                { src: 'Images/gf2.jpg', alt: 'Image 2' },
+                { src: 'Images/gf3.jpg', alt: 'Image 3' },
+                { src: 'Images/kodak1.jpg', alt: 'Image 4' },
+                { src: 'Images/kodak2.jpg', alt: 'Image 5' },
+                { src: 'Images/kodak3.jpg', alt: 'Image 6' },
+                { src: 'Images/me1.jpg', alt: 'Image 7' },
+                { src: 'Images/me2.jpg', alt: 'Image 8' },
+                { src: 'Images/me3.jpg', alt: 'Image 9' },
+            ]
+        };
+    }
+});
+
+
+new Vue({
+    el: '#app2',
+});
